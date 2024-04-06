1975eed7df22f0085e5a6f554aa96067f0cc6e0e
meenakshi-piraisudan <166209284+meenakshi-piraisudan@users.noreply.github.com>
2024-04-06 14:16:51 +0530

    Add files via upload
    week1

---
+++ week1/day1/FireFox.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		
+		int releaseYear= 1998;
+		float browserVersion=100.2f;
+		String browserName ="Firefox";
+		boolean isVisible= true;
+		char browserLogo='f';
+		System.out.println(releaseYear+"\n"+browserVersion+"\n"+browserName+"\n"+isVisible+"\n"+browserLogo);
+
+	}
+
+}
